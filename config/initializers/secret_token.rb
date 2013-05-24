6448ff298262bdf25bb54051eb396054b51a7cf2
pengweimin <bjmark@sina.com>
2013-05-24 10:35:36 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearnJquery::Application.config.secret_token = '288535584fcc23aaca139e405659443fa2d0488377d89ba57a3b7919ea06d57ae3a795fb67d53ec978896c97c110eb9fc25d6f636a108f33e5b63a51f7caf4ee'
